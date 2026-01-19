cc223a2e5dd9d660401352727f209672d250961c
Algomius <schlegel.philippe@free.fr>
2026-01-19 10:56:34 +0100

    Correctiond es routes

--- 01Endpoints/backend/main.py
+++ 01Endpoints/backend/main.py
@@ -12,27 +12,26 @@ app = FastAPI(title="API de gestion des tâches",
                 - Supprimer une tâche
               """)
 
-@app.get("/taches/", 
-        summary="Récupère toutes les tâches",
-        description="On récupère ici toutes les tâches de la base de données",
-        response_description="Liste des tâches au format JSON")
+@app.get("/taches/",
+         summary="Récupérer toutes les tâches",
+         description="Toutes les tâche de la BDD se retrouve dans un JSON",
+         response_description="Liste de toutes les tâche au format JSON")
+def getAllTache():
+    return {"message" :"Fonction qui va récupérer toutes les tâches"}
 
-def getTaches():
-    return {"message" : "Tu va récupérer la liste de toutes les tâches"}
-
-@app.get("/taches/{id}")
-def getTache(tache_id : int): 
-    return {"tache_id": tache_id, "titre": f"Tache {tache_id}"}
+@app.get("/taches/{tache_id}")
+def getTache(tache_id : int):
+    return {"message" : f"Récupération des infos de la tache {tache_id}"}
 
 @app.post("/taches/")
-def createTache(tache : Tache):
-    return {"id":1, "tache":tache}
+def createTache(t : Tache):
+    return {"message" : "Tache recue", "tache" : t}
 
-@app.put("/taches/{id}")
-def updateTache(tache : Tache):
-    return {"id":1, "tache":tache}
+@app.put("/taches/{tache_id}")
+def updateTache(tache_id : int, t : Tache):
+    return {"message" : f"Je vais modifier la tache {tache_id}", "nouvelleValeur": t}
 
-@app.delete("/taches/{id}")
-def deleteTache(tache_id : int): 
-    return {"tache_id": tache_id, "titre": f"Tache {tache_id}"}
+@app.delete("/taches/{tache_id}")
+def deleteTache(tache_id : int):
+    return {"message" : f"Je vais supprimer la tache {tache_id}"}
 
